3fec0d14dd1e618596695ec239b34d1394489e32
Katie Lentes <katielentes@noreply,github.com>
2025-03-16 10:34:17 -0500

    redir for tom

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,17 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/metropolis',
+        destination: 'https://validation.metropolis.io/84jnhk371l',
+        permanent: true,
+        basePath: false,
+      },
+
+    ]
+  },
 };
 
 export default nextConfig;
